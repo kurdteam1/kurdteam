d454df62ec186f3ab93ae011c17220fede6ab31a
KAK_ZED <71234040+968hacker@users.noreply.github.com>
2021-02-18 03:29:27 +0300

    Add files via upload

---
+++ fox.py
@@ -0,0 +1,18 @@
+import  os, sys , time
+os.system("clear")
+time.sleep(2)
+print("\033[31m\n          █████▒    ▒█████     ▒██   ██▒\n        ▓██   ▒    ▒██▒  ██▒   ▒▒ █ █▒░\n        ▒████ ░   ▒██░    ██▒    ░░█░\n        ░▓█▒  ░    ▒██   ██░    ░ █ █▒\n        ░▒█░       ░ ████▓▒░   ▒██▒▒██▒\n         ▒ ░       ░ ▒░▒░▒░    ▒▒ ░ ░▓ ░\n         ░           ░ ▒ ▒░    ░░   ░▒ ░\n         ░           ░ ░ ▒      ░    ░\n                       ░ ░      ░    ░\n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \033[0;1m     Auther: Zed\n      My lion: Dark\n      Chenal Telegram: kak_zed_1\n      Note: 30$ Hata Hatay / 10$ Bo Eak Mang\n      Note: ID Active a DllaKam \n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+while True:
+	h = "zed"
+	j = "fox"
+	jj = input(" Username: ")
+	hh = input(" Passowrd: ")
+	if h == hh and j == jj :
+		print("\033[32mAfarin :)")
+		time.sleep(1)
+		os.system("python2 .0.py")
+	else:
+		print("\033[33;1mHallaya !!")
+		time.sleep(1)
+		os.system("clear")
+		print("\n          █████▒    ▒█████     ▒██   ██▒\n        ▓██   ▒    ▒██▒  ██▒   ▒▒ █ █▒░\n        ▒████ ░   ▒██░    ██▒    ░░█░\n        ░▓█▒  ░    ▒██   ██░    ░ █ █▒\n        ░▒█░       ░ ████▓▒░   ▒██▒▒██▒\n         ▒ ░       ░ ▒░▒░▒░    ▒▒ ░ ░▓ ░\n         ░           ░ ▒ ▒░    ░░   ░▒ ░\n         ░           ░ ░ ▒      ░    ░\n                       ░ ░      ░    ░\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n      Auther: Zed\n      My lion: Dark\n      Chenal Telegram: kak_zed_1\n      Note: 30$ Hata Hatay / 10$ Bo Eak Mang\n      Note: ID Active a DllaKam \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
